88bcd9b92fd8775be3a3dfebaaf3ca85078f1b35
Lenovo-User <Lenovo-User@DESKTOP-RNPFAS5>
2023-10-16 09:32:52 +0200

    Add project files.

---
+++ 1.Swapping integers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Swapping_integers
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            int  a = 87, b = 45;   // numbers to be used         
+       Console.WriteLine("Before swap a= "+a+" b= "+b);    
+                   
+         //swapping two numbers  
+       Console.Write("After swap a= "+b+" b= "+a);
+                
+
+        }
+
+
+    }
+
+}
